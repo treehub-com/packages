cbf66b0234ac1bdb43c638753972c7077aceabf0
John Clark <socrates37@gmail.com>
2017-02-27 12:29:29 -0700

    list installed packages

--- components/package-manager-page/component.js
+++ components/package-manager-page/component.js
@@ -4,7 +4,22 @@ class Component extends core(HTMLElement) {
   constructor() {
     super({
       template: 'package-manager-page',
+      $: {
+        tbody: 'tbody',
+      },
     });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    const keys = Object.keys(window.packages);
+    keys.sort();
+    for (const key of keys) {
+      const pkg = window.packages[key];
+      const row = this.$.tbody.insertRow();
+      row.insertCell().insertAdjacentText('beforeend', pkg.name);
+      row.insertCell().insertAdjacentText('beforeend', pkg.version);
+    }
   }
 }
 
