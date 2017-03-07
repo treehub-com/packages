7d9ba376b5c702e7a0b3bd30ad09ad32b2733e73
John Clark <socrates37@gmail.com>
2017-03-07 14:28:16 -0700

    th-graphql; closes #13 (#14)

---
+++ components/th-graphql/component.js
@@ -0,0 +1,60 @@
+module.exports = (superclass) => class extends superclass {
+  constructor(args) {
+    superclass.name === 'HTMLElement' ? super() : super(args);
+  }
+
+  _query({url, query, input, type}) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: type ?
+          `query x($input: ${type}) {x:${query}}` : `query {x:${query}}`,
+        variables: JSON.stringify({
+          input,
+        }),
+      }),
+    })
+    .then((res) => res.json())
+    .then((json) => {
+      if (json.errors) {
+        const messages = [];
+        for (const error of json.errors) {
+          messages.push(error.message);
+        }
+        throw new Error(messages.join('\n'));
+      }
+      return json.data.x;
+    });
+  }
+
+  _mutation({url, query, input, type}) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `mutation x($input: ${type}) {
+          x:${query}
+        }`,
+        variables: JSON.stringify({
+          input,
+        }),
+      }),
+    })
+    .then((res) => res.json())
+    .then((json) => {
+      if (json.errors) {
+        const messages = [];
+        for (const error of json.errors) {
+          messages.push(error.message);
+        }
+        throw new Error(messages.join('\n'));
+      }
+      return json.data.x;
+    });
+  }
+};
