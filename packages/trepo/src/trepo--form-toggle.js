acf46b5844c77d262ce6f461a9978a6eaaae1cbb
John Clark <socrates37@gmail.com>
2017-04-26 08:54:40 -0600

    implemented form-toggle

--- packages/trepo/src/trepo--form-toggle.js
+++ packages/trepo/src/trepo--form-toggle.js
@@ -1,6 +1,45 @@
 class Component extends HTMLElement {
+  constructor() {
+    super();
+    this.addEventListener('click', () => this.clicked());
+  }
   connectedCallback() {
-    this.innerHTML = 'edit';
+    // Get the form element
+    this.form = this._findParentForm();
+
+    // Listen for enabled/disabled events
+    this.form.addEventListener('enabled', () => {
+      this.form.setAttribute('enabled', '');
+      this.textContent = 'cancel';
+    });
+    this.form.addEventListener('disabled', () => {
+      this.form.removeAttribute('enabled');
+      this.textContent = 'edit';
+    });
+
+    // Initialize our html
+    if (this.form.getAttribute('enabled')) {
+      this.textContent = 'cancel';
+    } else {
+      this.textContent = 'edit';
+    }
+  }
+  clicked() {
+    if (this.form.hasAttribute('enabled')) {
+      this.form.dispatchEvent(new Event('disabled'));
+    } else {
+      this.form.dispatchEvent(new Event('enabled'));
+    }
+  }
+
+  _findParentForm() {
+    let node = this.parentNode;
+    while (node) {
+      if (node.nodeName === 'FORM') {
+        return node;
+      }
+      node = node.parentNode;
+    }
   }
 }
 
