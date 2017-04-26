8fed195db7cdddebdc25dff74dba55448db5dd6d
John Clark <socrates37@gmail.com>
2017-04-26 14:59:48 -0600

    fleshing out person-data

--- packages/trepo/src/trepo-person-data.js
+++ packages/trepo/src/trepo-person-data.js
@@ -1,10 +1,25 @@
+import html from './trepo-person-data.html';
 import attr from '@thp/mixins/attr';
+import $ from '@thp/mixins/$';
 import {query} from '@thp/lib/graphql';
 
-class Component extends attr(HTMLElement) {
+const types = [
+  {name: 'Name', element: 'trepo-name'},
+  {name: 'Birth', element: 'trepo-birth'},
+  {name: 'Marriage', element: 'trepo-marriage'},
+  {name: 'Child', element: 'trepo-birth'}, // TODO determine where to set person
+  {name: 'Death', element: 'trepo-death'},
+];
+
+class Component extends attr($(HTMLElement)) {
   constructor() {
     super({
       attributes: Component.observedAttributes,
+      $: {
+        add: '.trepo-person-data-add',
+        data: 'trepo-person-data-data',
+        select: '.trepo-person-data-select',
+      },
     });
   }
 
@@ -16,7 +31,18 @@ class Component extends attr(HTMLElement) {
   }
 
   connectedCallback() {
+    this.innerHTML = html;
     super.connectedCallback();
+
+    // Add add eventhandler
+    this.$.add.addEventListener('click', () => this.add());
+
+    // Populate select
+    for (let type of types) {
+      const elem = `<option value="${type.element}">${type.name}</option>`;
+      this.$.select.insertAdjacentHTML('beforeend', elem);
+    }
+
     this._personChanged(this.person, null);
   }
 
@@ -39,8 +65,26 @@ class Component extends attr(HTMLElement) {
     name.person = id;
     name.node = person.name.id;
     name.value = person.name;
-    this.appendChild(name);
+    this.$.data.appendChild(name);
   }
+
+  add() {
+    const element = this.$.select.value;
+    const elem = document.createElement(element);
+
+    switch(element) {
+      case 'trepo-name':
+        elem.repo = this.repo;
+        elem.person = this.person;
+        break;
+    }
+
+    // TODO bind to create/"cancel" and move/delete element
+
+    this.$.data.insertAdjacentElement('afterbegin', elem);
+  }
+
+  // TODO a function to order the data elements
 }
 
 export default Component;
