a8a436b8723bf32d02b720d54f1d6462c0ea2816
John Clark <socrates37@gmail.com>
2017-04-28 14:45:33 -0600

    setting person mini cards

--- packages/tree/src/tree-person.js
+++ packages/tree/src/tree-person.js
@@ -9,6 +9,9 @@ class Component extends ref(attr(HTMLElement)) {
       attributes: Component.observedAttributes,
       html,
       $: {
+        children: 'tree-person-children',
+        spouses: 'tree-person-spouses',
+        parents: 'tree-person-parents',
         card: 'tree-person-card',
       },
     });
@@ -28,33 +31,108 @@ class Component extends ref(attr(HTMLElement)) {
   }
 
   _personChanged(newPerson, oldPerson) {
+    // TODO clear out any leftovers
+    this.$.card.innerHTML = '';
     this.layoutPage();
   }
 
   async layoutPage() {
-    const {name, birth, death} = await query({
+    const {births, marriages} = await query({
       url: `/api/${this.space}/${this.tree}`,
       query: `person(id: $input) {
-        name {name}
-        birth {date {original}}
-        death {date {original}}
+        births {
+          mother {id name {name} birth {date {original}} death {date {original}}}
+          father {id name {name} birth {date {original}} death {date {original}}}
+          child {id name {name} birth {date {original}} death {date {original}}}
+        }
+        marriages {
+          spouses {id name {name} birth {date {original}} death {date {original}}}
+        }
       }`,
       type: 'String',
       input: this.person,
     });
 
-    const personMini = document.createElement('trepo-person-mini');
-    personMini.value = {
-      name: (name) ? name.name : '(Unknown)',
-      url: `/tree/${this.space}/${this.tree}/${this.person}`,
-      birth: (birth && birth.date) ? birth.date.original : '?',
-      death: (death && death.date) ? death.date.original : '?',
-    };
+    // Marriages
+    if (marriages) {
+      for (let marriage of marriages) {
+        for (let spouse of marriage.spouses) {
+          if (spouse.id === this.person) {
+            continue;
+          }
+          const mini = document.createElement('trepo-person-mini');
+          mini.value = {
+            name: (spouse.name) ? spouse.name.name : '(Unknown)',
+            // TODO pull repo from person
+            url: `/tree/${this.space}/${this.tree}/${spouse.id}`,
+            birth: (spouse.birth && spouse.birth.date) ? spouse.birth.date.original : '?', // eslint-disable-line max-len
+            death: (spouse.death && spouse.death.date) ? spouse.death.date.original : '?', // eslint-disable-line max-len
+          };
+          this.$.spouses.appendChild(mini);
+        }
+      }
+    }
+
+    // Births
+    if (births) {
+      for (let birth of births) {
+        // Self
+        if (birth.child && birth.child.id === this.person) {
+          const mini = document.createElement('trepo-person-mini');
+          mini.value = {
+            name: (birth.child.name) ? birth.child.name.name : '(Unknown)',
+            // No url for self
+            birth: (birth.child.birth && birth.child.birth.date) ? birth.child.birth.date.original : '?', // eslint-disable-line max-len
+            death: (birth.child.death && birth.child.death.date) ? birth.child.death.date.original : '?', // eslint-disable-line max-len
+          };
+          this.$.card.appendChild(mini);
+        }
+
+        // Children
+        if ( ((birth.mother && birth.mother.id === this.person) ||
+              (birth.father && birth.father.id === this.person))
+            && birth.child) {
+          const mini = document.createElement('trepo-person-mini');
+          mini.value = {
+            name: (birth.child.name) ? birth.child.name.name : '(Unknown)',
+            // TODO pull repo from person
+            url: `/tree/${this.space}/${this.tree}/${birth.child.id}`,
+            birth: (birth.child.birth && birth.child.birth.date) ? birth.child.birth.date.original : '?', // eslint-disable-line max-len
+            death: (birth.child.death && birth.child.death.date) ? birth.child.death.date.original : '?', // eslint-disable-line max-len
+          };
+          this.$.children.appendChild(mini);
+        }
+        // Father
+        if (birth.child && birth.child.id === this.person && birth.father) {
+          const mini = document.createElement('trepo-person-mini');
+          mini.value = {
+            name: (birth.father.name) ? birth.father.name.name : '(Unknown)',
+            // TODO pull repo from person
+            url: `/tree/${this.space}/${this.tree}/${birth.father.id}`,
+            birth: (birth.father.birth && birth.father.birth.date) ? birth.father.birth.date.original : '?', // eslint-disable-line max-len
+            death: (birth.father.death && birth.father.death.date) ? birth.father.death.date.original : '?', // eslint-disable-line max-len
+          };
+          this.$.parents.appendChild(mini);
+        }
+        // Mother
+        if (birth.child && birth.child.id === this.person && birth.mother) {
+          const mini = document.createElement('trepo-person-mini');
+          mini.value = {
+            name: (birth.mother.name) ? birth.mother.name.name : '(Unknown)',
+            // TODO pull repo from person
+            url: `/tree/${this.space}/${this.tree}/${birth.father.id}`,
+            birth: (birth.mother.birth && birth.mother.birth.date) ? birth.mother.birth.date.original : '?', // eslint-disable-line max-len
+            death: (birth.mother.death && birth.mother.death.date) ? birth.mother.death.date.original : '?', // eslint-disable-line max-len
+          };
+          this.$.parents.appendChild(mini);
+        }
+      }
+    }
+
+    // Main Person Data
     const personData = document.createElement('trepo-person-data');
     personData.repo = `/api/${this.space}/${this.tree}`;
     personData.person = this.person;
-    this.$.card.innerHTML = '';
-    this.$.card.appendChild(personMini);
     this.$.card.appendChild(personData);
     // TODO listen for changes and update person-minis
   }
