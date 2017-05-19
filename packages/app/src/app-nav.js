4676e5ec85ea9c3626e7090ff9c6a712de7e02ed
John Clark <socrates37@gmail.com>
2017-05-19 08:33:02 -0600

    move nav to bottom on skinny screens; closes #36 (#40)

--- packages/app/src/app-nav.js
+++ packages/app/src/app-nav.js
@@ -1,6 +1,24 @@
-class Component extends HTMLElement {
+import attr from '@thp/mixins/attr';
+
+class Component extends attr(HTMLElement) {
+  constructor() {
+    super({
+      attributes: Component.observedAttributes,
+    });
+
+    this.elements = {};
+    this.selected = null;
+  }
+
+  static get observedAttributes() {
+    return [
+      'page',
+    ];
+  }
+
   connectedCallback() {
-    this.insertAdjacentHTML('beforeend', `<app-nav-logo></app-nav-logo>`);
+    super.connectedCallback();
+
     for (const name of Object.keys(window._.packages)) {
       const pkg = window._.packages[name];
       if (!pkg.provides || !pkg.provides.app || !pkg.provides.app.nav) {
@@ -9,9 +27,25 @@ class Component extends HTMLElement {
       const nav = pkg.provides.app.nav;
       if (Array.isArray(nav)) {
         for (const elem of nav) {
-          this.insertAdjacentHTML('beforeend', `<${elem}></${elem}>`);
+          const node = document.createElement(elem);
+          this.elements[name] = this.appendChild(node);
         }
       }
+    }
+  }
+
+  _pageChanged(newPage, oldPage) {
+    const parts = newPage.split('/').filter((s)=>s !== '');
+
+    // Remove selected
+    if (this.selected !== null) {
+      this.selected.classList.remove('app-nav-selected');
+    }
+
+    // If we have a matching nav item, select it
+    if (parts.length > 0 && this.elements[parts[0]] !== undefined) {
+      this.selected = this.elements[parts[0]];
+      this.selected.classList.add('app-nav-selected');
     }
   }
 }
--- packages/app/src/components.js
+++ packages/app/src/components.js
@@ -1,7 +1,7 @@
 import css from './style.css';
 import app from './app-app.js';
 import nav from './app-nav.js';
-import navLogo from './app-nav-logo.js';
+import logo from './app-logo.js';
 import space from './app-space.js';
 import taskbar from './app-taskbar.js';
 import page from './app-page.js';
@@ -10,7 +10,7 @@ import aside from './app-aside.js';
 document.head.insertAdjacentHTML('beforeend', `<style>${css}</style>`);
 window.customElements.define('app-app', app);
 window.customElements.define('app-nav', nav);
-window.customElements.define('app-nav-logo', navLogo);
+window.customElements.define('app-logo', logo);
 window.customElements.define('app-space', space);
 window.customElements.define('app-taskbar', taskbar);
 window.customElements.define('app-page', page);
